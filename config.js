dee9a5fffe718ec07db412f0777d466b7280f64a
look47 <46997648+look47@users.noreply.github.com>
2026-05-06 11:08:40 +0200

    Update APP_CONFIG with Google Sheets and emailjs
    Added Google Sheets URL and emailjs configuration.

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 const APP_CONFIG = {
-    // Aggiungi qui le date da bloccare
-    bookedDates: [
-        "2026-06-24", "2026-06-25", "2026-06-26", "2026-06-27",
-        "2026-08-03", "2026-08-04", "2026-08-05", "2026-08-06", 
-        "2026-08-07", "2026-08-08"
-    ],
+    googleSheetUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTdggamylvF2ppk6wdHX4tkPKF97eJRSL04x-mgT_dppkwqYJbfHp3gdgtgJFbfqeyb2xa17nDTISGZ/pub?gid=0&single=true&output=csv",
+    
+    // Lascia pure questi segnaposto per ora
+    emailjs: {
+        publicKey: "TUA_PUBLIC_KEY",
+        serviceId: "TUO_SERVICE_ID",
+        templateId: "TUO_TEMPLATE_ID"
+    },
+
     prices: { standard: 150, winter: 220 },
     markup: { 3: 1.15, 4: 1.30, 5: 1.45, 6: 1.60 },
     childExtra: 0.03,
